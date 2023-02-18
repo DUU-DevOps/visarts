be3373c7c6b8810be23e00430bc99e385a751ac6
Jerry Worthy <jdw102@duke.edu>
2023-02-18 17:48:30 -0500

    continued working artist spotlight page

--- src/components/artistSpotlight/ArtistSpotlight.js
+++ src/components/artistSpotlight/ArtistSpotlight.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {useState, useEffect} from 'react';
 import { createURL, grabImage } from '../sanityClient.js';
 import axios from 'axios';
-import {Grid, Avatar, Card, Typography, CardContent} from '@mui/material';
+import {Grid, Avatar, Card, Typography, CardContent, CardMedia, Paper} from '@mui/material';
 import { Carousel } from 'react-responsive-carousel';
-
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import '../../resources/artistSpotlight.css';
 
 const ArtistSpotlight = (props) => {
 
@@ -35,28 +35,32 @@ const ArtistSpotlight = (props) => {
 
     console.log(artistImages);
     return  (
-        <Grid container>
-            <Grid item xs = {6} component = {Card} raised>
-                <Avatar src ={artistInfo.image} />
-                <CardContent>
-                    {artistInfo.name}
-                </CardContent>
+        <div style = {{margin: 10}}>
+            <Grid container  alignItems = "flex-start" justifyContent = "space-evenly" >
+                <Grid style = {{margin: 10}} align = "center" item xs = {5} sx={{ fontFamily: 'monospace', backgroundColor: "#f5f3f2", padding: "10px"  }} component = {Paper}>
+                    <h1 className = "header">
+                        {artistInfo.name}
+                    </h1>
+                    <CardMedia >
+                        <Avatar src = {artistInfo.image}  sx = {{width: 300, height: 300, border: "5px solid #f5f3f2", marginBottom: "20px"}}/>
+                    </CardMedia>
+                    <Typography  variant='subtitle1' sx={{ fontFamily: "Playfair Display, serif", align: "center", backgroundColor: "#f5f3f2"  }}>
+                        {artistInfo.description}
+                    </Typography>
+                </Grid>
+                <Grid item xs = {6} style = {{margin: 10}} >
+                    <Carousel autoPlay = {true} infiniteLoop = {true}>
+                        {artistImages.map((img, key) => {
+                            return(
+                                <div style = {{height: "700px"}} key = {key}>
+                                    <img  alt = "" src = {img}/>
+                                </div>  
+                            )
+                        })}
+                    </Carousel>
+                </Grid>
             </Grid>
-            <Grid item xs = {6}  >
-                <Carousel autoPlay = {true} infiniteLoop = {true}>
-                    {artistImages.map((img, key) => {
-                        return(
-                            <div  key = {key}>
-                                <img style = {{height: "500px", width: "500px"}} alt = "" src = {img}/>
-                            </div>  
-                        )
-                    })}
-                 </Carousel>
-            </Grid>
-            {/* <Grid item xs = {12} component = {Card} raised>
-
-            </Grid> */}
-        </Grid>
+        </div>
     )
 }
 
