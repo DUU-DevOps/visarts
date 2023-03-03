ddcd9bb5ef632547e6b1a6f89dee8fc0de33e2ef
Jerry Worthy <jdw102@duke.edu>
2023-03-03 02:35:31 -0500

    continued editing artist spotlight page and begun creating homepage

--- src/components/artistSpotlight/ArtistSpotlight.js
+++ src/components/artistSpotlight/ArtistSpotlight.js
@@ -37,20 +37,19 @@ const ArtistSpotlight = (props) => {
     return  (
         <div style = {{marginTop: 70, marginLeft: 10, marginRight: 10}}>
             <Fade {...(artistInfo.name !== "" ? { timeout: 1000 } : {})}  in={artistInfo.name !== ""} mountOnEnter unmountOnExit>
-            <Grid container  alignItems = "flex-start" justifyContent = "space-evenly" >
-                    <Grid style = {{margin: 10}} align = "center" item xs = {12} md = {5} sx={{ fontFamily: 'monospace', padding: "10px" }} >
-                        <div className = "header">
-                            <h1 >
-                                {artistInfo.name}
-                            </h1>
-                        </div>
-                        
-                        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" style = {{marginTop: -100, marginBottom: -100}} className = "zoom" >
+            <Grid container  alignItems = "flex-start" justifyContent = "space-between" >
+                    <Grid style = {{margin: 10}} align = "center" item xs = {12} md = {5} sx={{ fontFamily: 'monospace', padding: "10px" }} >                       
+                        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" style = {{ marginBottom: -100, transform: "scale(1.1)",}} className = "filter" >
                             <defs>
                                 <clipPath id="user-space" clipPathUnits="userSpaceOnUse" >
-                                    <path  fill="#FF0066" d="M42.9,-61.4C56.4,-58.2,68.7,-47.5,77,-33.8C85.3,-20.1,89.6,-3.3,81.4,7.4C73.1,18.1,52.3,22.6,39,28.5C25.7,34.3,19.9,41.4,11.9,46.4C3.9,51.5,-6.5,54.5,-17.4,53.8C-28.4,53.2,-39.8,48.8,-47.6,40.7C-55.3,32.7,-59.4,20.9,-65.4,6.8C-71.4,-7.2,-79.4,-23.6,-72.7,-31C-65.9,-38.4,-44.3,-37,-29.8,-39.7C-15.2,-42.4,-7.6,-49.3,3.6,-54.8C14.7,-60.4,29.5,-64.6,42.9,-61.4Z" transform="translate(100 100)" />
+                                    <path  id = "curve" fill="#FF0066" d="M42.9,-61.4C56.4,-58.2,68.7,-47.5,77,-33.8C85.3,-20.1,89.6,-3.3,81.4,7.4C73.1,18.1,52.3,22.6,39,28.5C25.7,34.3,19.9,41.4,11.9,46.4C3.9,51.5,-6.5,54.5,-17.4,53.8C-28.4,53.2,-39.8,48.8,-47.6,40.7C-55.3,32.7,-59.4,20.9,-65.4,6.8C-71.4,-7.2,-79.4,-23.6,-72.7,-31C-65.9,-38.4,-44.3,-37,-29.8,-39.7C-15.2,-42.4,-7.6,-49.3,3.6,-54.8C14.7,-60.4,29.5,-64.6,42.9,-61.4Z" transform="translate(100 100)" />
                                 </clipPath>
                             </defs>
+                            <text x="320" dy = "-10" >
+                            <textPath xlinkHref="#curve" className = "header" >
+                                    {artistInfo.name}
+                            </textPath>
+                            </text>
                             <image width="100%" height="100%"  xlinkHref= {artistInfo.image} clipPath= "url(#user-space)"/>
                         </svg>
 
--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import {Grid} from '@mui/material';
-import {TransitionGroup} from 'react-transition-group'; // ES6
+import {Grid, Avatar} from '@mui/material';
 import  '../../resources/homepage.css';
-
+import { createURL, grabImage } from '../sanityClient.js';
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 
 const Homepage = (props) => {
+    const [content, setContent] = useState({});
+    const HOMEPAGE_URL = createURL("homepage");
+    useEffect(() => {
+        axios.get(HOMEPAGE_URL)
+        .then((response) => {
+            console.log(response);
+            setContent({
+                statement: response.data.result[0].statement,
+                image: grabImage(response.data.result[0].image.asset)
+            })
+        });
+    }, [HOMEPAGE_URL]);
+    console.log(content);
     return  (
-        <div>
-            
+        <div > 
+            <Grid container justifyContent="center">
+                <Grid item xs = {12} justifyContent= "center">
+                    <div style = {{position: "relative", backgroundImage: `url(${content.image})`, backgroundSize: "100% 100%", width: "100%", height: "640px"}}>
+                        <div style = {{width: "600px", height: "300px", backgroundColor: "#89c3db", position: "absolute", marginTop: 250}}>
+                            <Grid container style = {{marginTop: "3%", marginLeft: "3%"}}>
+                                <Grid item xs = {5}>
+                                    <Avatar variant = "square" style = {{width: "250px", height: "250px",  borderRadius: "40px"}} src ={require("../../resources/logo.png")}/>
+                                </Grid>
+                                <Grid item xs ={6} >
+                                    <div className = "statement" style = {{marginTop: 50}}>
+                                        <h1 style = {{fontSize: "22px"}} >Experience the world through <em>your</em> art. </h1>
+                                        <p>{content.statement}</p>
+                                    </div>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </div>
+                </Grid> 
+            </Grid>
         </div>
     )
 }
