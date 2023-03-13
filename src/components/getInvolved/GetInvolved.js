c0521739bdec5be8eff0c0fcbe92e5869695ed88
Jerry Worthy <jdw102@duke.edu>
2023-03-13 13:31:18 -0400

    created basic design for meet us and alumni page

--- src/components/getInvolved/GetInvolved.js
+++ src/components/getInvolved/GetInvolved.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {useState, useEffect } from 'react';
 import { createURL, grabImage } from '../sanityClient.js';
 import axios from 'axios';
-import {Grid,  Fade, Link,  Avatar, Card, CardContent, CardMedia, Button } from '@mui/material';
+import {Grid,  Fade, Link,  Avatar, CardContent, CardMedia, Button, Grow } from '@mui/material';
 import '../../resources/getInvolved.css';
+
 
 
 
@@ -19,13 +20,16 @@ const GetInvolved = (props) => {
         .then((response) => {
             let temp = response.data.result[0];
             let arr = [];
+            let delay = 0;
             temp.contactLinks.forEach((link) => {
+                delay =  delay + 500;
                 arr.push({
                     name: link.name,
                     url: link.url,
                     icon: grabImage(link.icon),
                     button: link.buttonTitle,
-                    copy: link.clickCopy
+                    copy: link.clickCopy,
+                    del: delay
                 })
             })
             setContent({
@@ -54,27 +58,29 @@ const GetInvolved = (props) => {
                 <Grid container justifyContent = "space-evenly" style = {{marginTop: "20px", marginBottom: "10px"}}>
                     {content.contacts.map((c) => {
                         return (
-                            <Grid item xs = {12} sm = {3}  style = {{justifyContent: 'center', display: 'flex'}} raised component = {Card}>
-                                    <CardContent>
-                                        <CardMedia>
-                                            <Avatar className = "zoom" sx={{ width: 200, height:200, display: "flex", justifyContent: "center"}} src = {c.icon} />
-                                        </CardMedia>
-                                        <h3 className = "header">
-                                            {c.name}
-                                        </h3>
-                                        <Link to = {c.url} style = {{textDecoration: 'none', display: "flex", justifyContent: "center"}}  >
-                                            <Button
-                                                variant = "contained"
-                                                sx={{  ':hover': {
-                                                    bgcolor: '#efaf40', 
-                                                    color: 'white'}, my: 2, color: "white", display: 'block',  textTransform: 'none', backgroundColor: "#815299", borderRadius: "20px" }}
-                                                 onClick={() => {c.copy ? navigator.clipboard.writeText(c.url): console.log("")}} className = "text"
-                                            >
-                                                {c.button}
-                                            </Button>
-                                        </Link>
-                                    </CardContent>
-                            </Grid>
+                            <Grow in = {content.contacts.length > 0} {...(content.contacts.length > 0 ? { timeout: c.del} : {})}>
+                                <Grid item xs = {12} sm = {3}  style = {{justifyContent: 'center', display: 'flex'}} >
+                                        <CardContent>
+                                            <CardMedia>
+                                                <Avatar className = "zoom" sx={{ width: 200, height:200, display: "flex", justifyContent: "center"}} src = {c.icon} />
+                                            </CardMedia>
+                                            <h3 className = "header">
+                                                {c.name}
+                                            </h3>
+                                            <Link to = {c.url} style = {{textDecoration: 'none', display: "flex", justifyContent: "center"}}  >
+                                                <Button
+                                                    variant = "contained"
+                                                    sx={{  ':hover': {
+                                                        bgcolor: '#efaf40', 
+                                                        color: 'white'}, my: 2, color: "white", display: 'block',  textTransform: 'none', backgroundColor: "#815299", borderRadius: "20px" }}
+                                                        onClick={() => {c.copy ? navigator.clipboard.writeText(c.url): console.log("")}} className = "text"
+                                                >
+                                                    {c.button}
+                                                </Button>
+                                            </Link>
+                                        </CardContent>
+                                </Grid>
+                            </Grow>
                         )
                     })}
                 </Grid>
--- src/components/meetUs/MeetUs.js
+++ src/components/meetUs/MeetUs.js
@@ -1,8 +1,54 @@
+import { createURL, grabImage } from '../sanityClient.js';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import React from 'react';
+import {Grid, Avatar, Grow } from '@mui/material';
+
 
 const MeetUs = (props) => {
+    const PEOPLE_URL = createURL("people");
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        axios.get(PEOPLE_URL)
+        .then((response) => {
+            let temp = [];
+            let delay = 0;
+            response.data.result.forEach((a) => {
+                delay = delay + 1000;
+                temp.push({
+                    name: a.name,
+                    title: a.title,
+                    image: grabImage(a.image.asset),
+                    del: delay
+                })
+            }
+            )
+            setPeople(temp);
+        });
+    }, [PEOPLE_URL]);
+
+
     return  (
         <div>
-            
+            <Grid container style = {{marginTop: "80px"}} spacing = {3} justifyContent = "space-evenly">
+                {people.map((a) => {
+                    return (
+                        <Grow in = {people.length > 0} {...(people.length > 0 ? { timeout: a.del} : {})} >
+                            <Grid  item xs = {4} >
+                                <div style = {{display: "flex", justifyContent: "center"}}>
+                                    <Avatar variant = "square" sx={{ width: 300, height:300}} src = {a.image} />
+                                </div>
+                                <h3 className = "header" >
+                                    {a.title} 
+                                    <br />
+                                    {a.name}
+                                </h3>
+                            </Grid>
+                        </Grow>
+                    )
+                })}
+            </Grid>
         </div>
     )
 }
